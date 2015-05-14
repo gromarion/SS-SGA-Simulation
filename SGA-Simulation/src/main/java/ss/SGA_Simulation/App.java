3c68eee8b8bfe7125f7f1b0575d05d0bd1de6ad0
German Romarion <grromarion@hotmail.com>
2015-05-14 10:56:38 -0300

    Created Maven Project.

---
+++ SGA-Simulation/src/main/java/ss/SGA_Simulation/App.java
@@ -0,0 +1,13 @@
+package ss.SGA_Simulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
