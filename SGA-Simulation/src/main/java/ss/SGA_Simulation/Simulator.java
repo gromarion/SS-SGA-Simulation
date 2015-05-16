efaecd8a3ffd9f85b1ebbde46a457bd7a55b92cb
German Romarion <grromarion@hotmail.com>
2015-05-16 12:50:49 -0300

    Added printf that shows the carreer plan

--- SGA-Simulation/src/main/java/ss/SGA_Simulation/Simulator.java
+++ SGA-Simulation/src/main/java/ss/SGA_Simulation/Simulator.java
@@ -6,6 +6,7 @@ public class Simulator {
 	public static void main(String[] args) {
 		Carreer carreer = CarreerParser.parse("SoftwareEngineering.xml");
 		List<Student> students = StudentsCreator.create(carreer, 100);
+		System.out.println(carreer);
 		for (Student student : students) {
 			System.out.println(student);
 		}
