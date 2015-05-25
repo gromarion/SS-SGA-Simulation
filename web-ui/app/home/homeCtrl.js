da4bad6deac9e48200784d06f4b65dbedd491aff
Franco Arolfo <francoarolfo@hotmail.com>
2015-05-25 06:36:04 -0300

    adding days graph

--- web-ui/app/home/homeCtrl.js
+++ web-ui/app/home/homeCtrl.js
@@ -2,51 +2,90 @@
 
 angular.module('sgaSimulator.controllers')
 
-.controller('HomeCtrl', ['$scope', '$state', function($scope, $state) {
-    $scope.snapshot = [
+.controller('HomeCtrl', ['$scope', '$state', '$timeout', function($scope, $state, $timeout) {
+
+    var maxPx = jQuery(document.getElementsByClassName('students-graph')[0]).height();
+
+    function calcHeight(studentsValue, maxStudents) {
+        return studentsValue * maxPx / maxStudents;
+    }
+
+    function updateElemHeight(section, maxStudents) {
+      debugger
+        section.elem.style.height = calcHeight(section.value, maxStudents) + 'px';
+    };
+
+    $scope.updateMatriculationGraph = function(snapshot) {
+        snapshot.days.forEach(function(day) {
+            var barSections = [
+              {
+                elem: document.getElementById('#' + day.title + '-success'),
+                value: day.matriculationSuccess
+              },{
+                elem: document.getElementById('#' + day.title + '-fail'),
+                value: day.matriculationFail
+              },{
+                elem: document.getElementById('#' + day.title + '-conflict'),
+                value: day.matriculationConflict
+              }
+            ];
+
+            _.map(barSections, function(section) {
+              updateElemHeight(section, snapshot.maxStudents);
+            });
+        });
+    };
+
+    $scope.snapshot = {
+      maxStudents: 10,
+      days: [
       {
-        title: 'LUN',
-        matriculationSuccess: 8,
-        matriculationConflict: 1,
+        title: 'MON',
+        matriculationSuccess: 1,
+        matriculationConflict: 4,
         matriculationFail: 2
       },
       {
-        title: 'MAR',
+        title: 'TUE',
         matriculationSuccess: 5,
         matriculationConflict: 3,
         matriculationFail: 0
       },
       {
-        title: 'MIE',
+        title: 'WED',
         matriculationSuccess: 1,
         matriculationConflict: 3,
         matriculationFail: 2
       },
       {
-        title: 'JUE',
-        matriculationSuccess: 4,
+        title: 'THU',
+        matriculationSuccess: 3,
         matriculationConflict: 3,
-        matriculationFail: 2
+        matriculationFail: 0
       },
       {
-        title: 'VIE',
-        matriculationSuccess: 4,
-        matriculationConflict: 3,
-        matriculationFail: 2
+        title: 'FRI',
+        matriculationSuccess: 1,
+        matriculationConflict: 1,
+        matriculationFail: 0
       },
       {
-        title: 'SAB',
-        matriculationSuccess: 4,
-        matriculationConflict: 3,
-        matriculationFail: 2
+        title: 'SAT',
+        matriculationSuccess: 0,
+        matriculationConflict: 0,
+        matriculationFail: 0
       },
       {
-        title: 'DOM',
-        matriculationSuccess: 4,
-        matriculationConflict: 3,
-        matriculationFail: 2
-      }
-    ];
+        title: 'SUN',
+        matriculationSuccess: 0,
+        matriculationConflict: 0,
+        matriculationFail: 0
+      }]
+    };
 
-    $scope.selected = 'LUN';
+    $scope.selected = 'FRI';
+
+    $timeout(function() {
+      $scope.updateMatriculationGraph($scope.snapshot);
+    }, 2000);
 }]);
